cfce2dc201ac8ebbcca4b67fa2b1f368b9bd5a52
Brian Reigersberg <brianreigersberg@gmail.com>
2021-04-11 08:12:34 +0200

    fixed routing

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const http = require("http");
 const socketio = require('socket.io');
+const path = require('path');
 
 const Participant = require('./participant');
 
@@ -10,7 +11,11 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-app.use('/*', express.static('public'));
+app.use(express.static('public'));
+
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/public/index.html'));
+});
 
 // console.log that your server is up and running
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
